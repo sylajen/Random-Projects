ae1e9b67608c0f8e1374d0a058e09390b5adad25
sylajen <33578533+sylajen@users.noreply.github.com>
2023-01-27 15:57:23 -0500

    Update phonetoword.py
    Renamed functions and arrays for clarity.
    Created a function that converts existing array into an array that doesn't contain zeros.

--- PhoneNumber to Word Converter/phonetoword.py
+++ PhoneNumber to Word Converter/phonetoword.py
@@ -2,28 +2,41 @@
 # 2 - ABC | 3 - DEF | 4 - GHI | 5 - JKL | 6 - MNO | 7 - PQRS | 8 - TUV | 9 - WXYZ 
 # 8 functions, one for each number
 userInput = input("Convert from number to letters, phone style: ")
-initialNumber = [int(i) for i in str(userInput)]
-totalPos = len(initialNumber) - 1 #total positions = total numbers - 1
-ansPossible = [] #empty array to house number of possibilites per number
+initialNumber = [int(i) for i in str(userInput)] #puts initial number into an array
+totalLength = len(initialNumber) #total number of digits
+totalPositions = totalLength - 1 #total positions = total digits - 1
+ansPossibleArr = [] #empty array to house number of possibilites per number
 
 #this function will look through all digits of the number inputted, then assigns the number of possibilities per digit, then calculcates the answers possible
-def answersPossible(initialNumber, ansPossible, totalPos):
+def answersPossibleFunc():
     checker = 0
     #create an array with the number of possibilities for each number
-    for checker in range(len(initialNumber)):
+    for checker in range(totalLength):
         if initialNumber[checker] == 7 or initialNumber[checker] == 9:
-            ansPossible.append(4) #4 letters for these numbers
+            ansPossibleArr.append(4) #4 letters for these numbers
         elif initialNumber[checker] == 2 or initialNumber[checker] == 3 or initialNumber[checker] == 4 or initialNumber[checker] == 5 or initialNumber[checker] == 6 or initialNumber[checker] == 8:
-            ansPossible.append(3) #3 letters for these numbers
+            ansPossibleArr.append(3) #3 letters for these numbers
         else:
-            ansPossible.append(0) #0 letters for these numbers
+            ansPossibleArr.append(0) #0 letters for these numbers
 
 #call the previously defined function and insert everything we've gathered
-answersPossible(initialNumber, ansPossible, totalPos)
-print("There are", len(initialNumber), "total numbers")
-            
-print("These are how many possible letters there are per number: ", ansPossible)
-    
+answersPossibleFunc()
+print("There are", totalLength, "total digits")
+print("These are the possible number of letters per digit: ", ansPossibleArr)
+
+#create a function to convert array into non-zero array
+nonzeroArr = []
+def nonzeroArrayFunc():
+    checker2 = 0
+    for checker2 in range(totalLength):
+        if ansPossibleArr[checker2] != 0:
+            nonzeroArr.append(ansPossibleArr[checker2])
+        else:
+            pass
+
+#call the previously defined function and insert everything we've gathered
+nonzeroArrayFunc()
+print("The same array without 0 values is: ", nonzeroArr)
 
         
     
