5c008d5d45e094e7d8864c761a2c95ebddbbadae
sylajen <33578533+sylajen@users.noreply.github.com>
2023-01-31 19:30:39 -0500

    Update phonetoword.py
    Fixed the total outcome calculation
    Added variables and arrays for each number value

--- PhoneNumber to Word Converter/phonetoword.py
+++ PhoneNumber to Word Converter/phonetoword.py
@@ -1,5 +1,17 @@
 # Convert Numbers to letters based on telephone numbers
 # 2 - ABC | 3 - DEF | 4 - GHI | 5 - JKL | 6 - MNO | 7 - PQRS | 8 - TUV | 9 - WXYZ 
+#create something for each alphanumeric association
+varZero   = " "
+varOne    = " "
+arrTwo    = ["A", "B", "C"]
+arrThree  = ["D", "E", "F"]
+arrFour   = ["G", "H", "I"]
+arrFive   = ["J", "K", "L"]
+arrSix    = ["M", "N", "O"]
+arrSeven  = ["P", "Q", "R", "S"]
+arrEight  = ["T", "U", "V"]
+arrNine   = ["W", "X", "Y", "Z"]
+
 # 8 functions, one for each number
 userInput = input("Convert from number to letters, phone style: ")
 initialNumber = [int(i) for i in str(userInput)] #puts initial number into an array
@@ -40,10 +52,10 @@ print("Zeroes represent digits without letters; these will be represented as spa
 print("The same array without 0 values is: ", nonzeroArr)
 
 #create a function that will calculate the total number of outcomes from the user entered number
-totalOutcomes = nonzeroArr[0]
+totalOutcomes = 1
 def totalOutcomesFunc():
     #literally just have to multiply the the values in the nonzeroArr
-    checker3 = 1
+    checker3 = 0
     global totalOutcomes
     for checker3 in range(len(nonzeroArr)):
         totalOutcomes = totalOutcomes * nonzeroArr[checker3]  #NOT CORRECT YET, it multiplies the first array value twice for some reason
@@ -51,29 +63,10 @@ def totalOutcomesFunc():
 #call the totalOutcome function and display the total outcomes
 totalOutcomesFunc()
 print("The total amount of outcomes possible from the provided number is", totalOutcomes)
-    
-        
-    
-# def two():          #ABC
-# def three():        #DEF
-# def four():         #GHI
 
-# def five():         #JKL
-    
-# def six():          #MNO
+#use the total outcomes to create the array of outcomes
+totalOutcomesArr[totalOutcomes]
 
-# def seven():        #PQRS
-    
-# def eight():        #TUV
-    
-# def nine():         #WXYZ
-    
-# def mainFunc(userInput):
-    
-#     #for xRuns < totalX:
-        
-#     print(convertedString)
+#go through all the possible options
 
-
-# totalX = total[]
 
